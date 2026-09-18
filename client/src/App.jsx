0f2151136d858e3b61b41ee2aa20a068f4329de3
arjanbandyopadhyay <arjanbandyopadhyay247@gmail.com>
2026-09-18 12:46:51 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import InterviewHistory from "./pages/interviewHistory"
 import InterviewReport from "./pages/InterviewReport"
 import Pricing from "./pages/Pricing"
 
-export const ServerUrl = "http://localhost:8000"
+export const ServerUrl = "https://ai-interview-platfrom.onrender.com"
 
 function App() {
   const dispatch=useDispatch()
